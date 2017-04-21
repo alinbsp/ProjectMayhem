03e8143a88d7687038859668937b7d6384c82c14
Alin Iacob <alinbsp@gmail.com>
2017-04-21 12:12:03 -0700

    Add vanity banner

--- tracker/mayhem/src/main/java/org/mayhem/MayhemApplication.java
+++ tracker/mayhem/src/main/java/org/mayhem/MayhemApplication.java
@@ -1,10 +1,20 @@
 package org.mayhem;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@EnableAutoConfiguration
 public class MayhemApplication {
+
+	@RequestMapping("/")
+    String home() {
+	    return "Mayhem";
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(MayhemApplication.class, args);
