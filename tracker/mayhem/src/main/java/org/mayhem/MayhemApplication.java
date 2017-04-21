030a413b7c7f502306e56167ae42005ef77e7727
Alin Iacob <alinbsp@gmail.com>
2017-04-21 11:12:28 -0700

    Tracker init

---
+++ tracker/mayhem/src/main/java/org/mayhem/MayhemApplication.java
@@ -0,0 +1,12 @@
+package org.mayhem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MayhemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MayhemApplication.class, args);
+	}
+}
---
+++ tracker/mayhem/src/test/java/org/mayhem/MayhemApplicationTests.java
@@ -0,0 +1,16 @@
+package org.mayhem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MayhemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
